030aeb83ffb6b1d4d65ae26bacad51cf989f45fd
VictorGithige <muhuthiagithige1@gmail.com>
2023-03-20 10:06:52 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+
+
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    }
+  }
+  
+  const encouragingPromptFunction = wrapAdjective("!!!");
+encouragingPromptFunction("a dedicated programmer"); // "You are !!!a dedicated programmer!!!"
